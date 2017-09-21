4c974b653c5bcd4fc7cf9df9ad84f124aae260ad
ladannasserian <ladan@segment.com>
2017-09-21 12:12:09 -0700

    Refactor logic, check integration.options instead of traits, rename CONST

--- Segment-WebEngage/Classes/WEGSegmentIntegration.m
+++ Segment-WebEngage/Classes/WEGSegmentIntegration.m
@@ -7,7 +7,7 @@
 //
 
 #import "WEGSegmentIntegration.h"
-
+#import <Analytics/SEGAnalyticsUtils.h>
 
 @implementation WEGSegmentIntegration
 
@@ -20,102 +20,84 @@
     
     WEGUser* user = [WebEngage sharedInstance].user;
     
-    if (payload.userId != nil && [payload.userId length] != 0) {
+    if (payload.userId) {
         [user login:payload.userId];
-        NSLog(@"[[WebEngage sharedInstance].user login:%@]", payload.userId);
+        SEGLog(@"[[WebEngage sharedInstance].user login:%@]", payload.userId); // example Segment Log - please add to the integration
     }
     
+    NSDictionary* traits = payload.traits;
+    if (!traits) {
+        return;
+    }
     NSMutableDictionary* traitsCopy = [payload.traits mutableCopy];
     
-    NSDictionary* traits = payload.traits;
+    // While I don't believe this is necessary, we can ensure the value is of type NSString with stringValue
+    NSString* firstName = [traits[WEG_SEGMENT_FIRST_NAME_KEY] stringValue];
+    if (firstName) {
+        [user setFirstName:firstName];
+    }
     
-    if (traits) {
-        if (traits[WEG_SEGMENT_FIRST_NAME_KEY] && [traits[WEG_SEGMENT_FIRST_NAME_KEY] isKindOfClass:[NSString class]]
-            &&((NSString*)traits[WEG_SEGMENT_FIRST_NAME_KEY]).length > 0) {
-            
-            [user setFirstName:traits[WEG_SEGMENT_FIRST_NAME_KEY]];
+    NSString* lastName = [traits[WEG_SEGMENT_LAST_NAME_KEY] stringValue];
+    if (lastName) {
+        [user setLastName:lastName];
+    }
+    
+    // I wasn't sure of what you were trying to accomplish here, but my understanding was - if
+    // there is no firstName or lastName, retrieve name. Split name into firstName and lastName
+    // then set it
+    if (!firstName && !lastName) {
+        
+        NSString* name = [traits[WEG_SEGMENT_NAME_KEY] stringValue];
+        if (!name) {
+            return;
+        }
+        NSArray* nameComponents = [name componentsSeparatedByString:@" "];
+        if(nameComponents &&  nameComponents.count > 0) {
+            firstName = nameComponents[0];
+            [traitsCopy removeObjectForKey:WEG_SEGMENT_NAME_KEY];
         }
         
-        if (traits[WEG_SEGMENT_LAST_NAME_KEY] && [traits[WEG_SEGMENT_LAST_NAME_KEY] isKindOfClass:[NSString class]]
-            && ((NSString*)traits[WEG_SEGMENT_LAST_NAME_KEY]).length > 0) {
-            [user setLastName:traits[WEG_SEGMENT_LAST_NAME_KEY]];
+        if (nameComponents && nameComponents.count > 1) {
+            lastName = nameComponents[nameComponents.count-1];
         }
         
-        if (!(traits[WEG_SEGMENT_FIRST_NAME_KEY]
-              && [traits[WEG_SEGMENT_FIRST_NAME_KEY] isKindOfClass:[NSString class]]
-              && ((NSString*)traits[WEG_SEGMENT_FIRST_NAME_KEY]).length > 0)
-            && !(traits[WEG_SEGMENT_LAST_NAME_KEY]
-                 && [traits[WEG_SEGMENT_LAST_NAME_KEY] isKindOfClass:[NSString class]]
-                 && ((NSString*)traits[WEG_SEGMENT_LAST_NAME_KEY]).length > 0)) {
-            
-            id name = traits[WEG_SEGMENT_NAME_KEY];
-            if(name && [name isKindOfClass:[NSString class]] && [name length] > 0) {
-                NSArray* nameComponents = [name componentsSeparatedByString:@" "];
-                NSString* firstName = nil;
-                NSString* lastName = nil;
-                if(nameComponents &&  nameComponents.count > 0) {
-                    
-                    firstName = nameComponents[0];
-                    [traitsCopy removeObjectForKey:WEG_SEGMENT_NAME_KEY];
-                }
-                
-                if (nameComponents && nameComponents.count > 1) {
-                    lastName = nameComponents[nameComponents.count-1];
-                }
-                
-                if (firstName && firstName.length > 0) {
-                    [user setFirstName:firstName];
-                    
-                }
-                
-                if (lastName && lastName.length > 0) {
-                    [user setLastName:lastName];
-                }
-            }
-            
+        if (firstName && firstName.length > 0) {
+            [user setFirstName:firstName];
         }
         
-        
-        if (traits[WEG_SEGMENT_EMAIL_KEY] && [traits[WEG_SEGMENT_EMAIL_KEY] isKindOfClass:[NSString class]]
-            && ((NSString*)traits[WEG_SEGMENT_EMAIL_KEY]).length > 0) {
-            [user setEmail:traits[WEG_SEGMENT_EMAIL_KEY]];
+        if (lastName && lastName.length > 0) {
+            [user setLastName:lastName];
         }
         
-        if (traits[WEG_SEGMENT_GENDER_KEY] && [traits[WEG_SEGMENT_GENDER_KEY] isKindOfClass:[NSString class]]
-            && ((NSString*)traits[WEG_SEGMENT_GENDER_KEY]).length > 0) {
-            
-            NSString* gender = traits[WEG_SEGMENT_GENDER_KEY];
-            if([gender caseInsensitiveCompare:@"male"] == NSOrderedSame ||
-               [gender caseInsensitiveCompare:@"m"] == NSOrderedSame) {
+        NSString* email = [traits[WEG_SEGMENT_EMAIL_KEY] stringValue];
+        if (email) {
+            [user setEmail:email];
+        }
+        
+        NSString* gender = [traits[WEG_SEGMENT_GENDER_KEY] stringValue];
+        if (gender) {
+            if([gender caseInsensitiveCompare:@"male"] == NSOrderedSame || [gender caseInsensitiveCompare:@"m"] == NSOrderedSame) {
                 [user setGender:@"male"];
-            } else if([gender caseInsensitiveCompare:@"female"] == NSOrderedSame ||
-                      [gender caseInsensitiveCompare:@"f"] == NSOrderedSame) {
+            } else if([gender caseInsensitiveCompare:@"female"] == NSOrderedSame || [gender caseInsensitiveCompare:@"f"] == NSOrderedSame) {
                 [user setGender:@"female"];
-            } else if([gender caseInsensitiveCompare:@"other"] == NSOrderedSame ||
-                      [gender caseInsensitiveCompare:@"others"] == NSOrderedSame) {
+            } else if([gender caseInsensitiveCompare:@"other"] == NSOrderedSame || [gender caseInsensitiveCompare:@"others"] == NSOrderedSame) {
                 [user setGender:@"other"];
             }
         }
         
-        if (traits[WEG_SEGMENT_COMPANY_KEY] && [traits[WEG_SEGMENT_COMPANY_KEY] isKindOfClass:[NSString class]]
-            &&((NSString*)traits[WEG_SEGMENT_COMPANY_KEY]).length > 0) {
-            
-            [user setCompany:traits[WEG_SEGMENT_COMPANY_KEY]];
+        NSString* company = [traits[WEG_SEGMENT_COMPANY_KEY] stringValue];
+        if (company) {
+            [user setCompany:company];
         }
         
-        if (traits[WEG_SEGMENT_PHONE_KEY] && [traits[WEG_SEGMENT_PHONE_KEY] isKindOfClass:[NSString class]]
-            && ((NSString*)traits[WEG_SEGMENT_PHONE_KEY]).length > 0) {
+        NSString* phone = [traits[WEG_SEGMENT_PHONE_KEY] stringValue];
+        if (phone) {
             [user setPhone:traits[WEG_SEGMENT_PHONE_KEY]];
         }
 
-        
-        if (traits[WEG_SEGMENT_BIRTH_DATE_KEY]) {
-            
-            
-            id birthDay = traits[WEG_SEGMENT_BIRTH_DATE_KEY];
-            
+        id birthDay = traits[WEG_SEGMENT_BIRTH_DATE_KEY];
+        if (birthDay) {
             NSDate* date = nil;
-            
             if ([birthDay isKindOfClass:[NSNumber class]] ) {
                 //assumption is Unix Timestamp
                 date = [NSDate dateWithTimeIntervalSince1970:((NSNumber*)birthDay).longValue/1000];
@@ -123,13 +105,11 @@
                 //assumption is ISO Date format
                 NSISO8601DateFormatter *dateFormat = [[NSISO8601DateFormatter alloc] init];
                 date = [dateFormat dateFromString:birthDay];
-                
             } else if ([birthDay isKindOfClass:[NSDate class]]) {
                 date = birthDay;
             }
             
             if (date) {
-                
                 NSDateFormatter* birthDateFormatter = [[NSDateFormatter alloc] init];
                 [birthDateFormatter setDateFormat:@"yyyy-MM-dd"];
                 [birthDateFormatter setTimeZone:[NSTimeZone timeZoneWithAbbreviation:@"UTC"]];
@@ -139,34 +119,36 @@
             
         }
         
-        if (traits[WEG_HASHED_EMAIL_KEY] && [traits[WEG_HASHED_EMAIL_KEY] isKindOfClass:[NSString class]]
-            && ((NSString*)traits[WEG_HASHED_EMAIL_KEY]).length > 0) {
-            [user setHashedEmail:traits[WEG_HASHED_EMAIL_KEY]];
+        
+        NSDictionary* integration = [payload.integrations valueForKey:@"WebEngage"];
+        NSString* wegHashEmail = [integration[WEG_HASHED_EMAIL_KEY] stringValue];
+        if (wegHashEmail) {
+            [user setHashedEmail:wegHashEmail];
         }
         
-        if (traits[WEG_HASHED_PHONE_KEY] && [traits[WEG_HASHED_PHONE_KEY] isKindOfClass:[NSString class]]
-            && ((NSString*)traits[WEG_HASHED_PHONE_KEY]).length > 0) {
-            [user setHashedPhone:traits[WEG_HASHED_PHONE_KEY]];
+        NSString* wegHashedPhoneKey = [integration[WEG_HASHED_PHONE_KEY] stringValue];
+        if (wegHashedPhoneKey) {
+            [user setHashedPhone:wegHashedPhoneKey];
         }
         
-        if (traits[WEG_PUSH_OPT_IN_KEY] && [traits[WEG_PUSH_OPT_IN_KEY] isKindOfClass:[NSNumber class]]) {
-            [user setOptInStatusForChannel:WEGEngagementChannelPush status:[traits[WEG_PUSH_OPT_IN_KEY] boolValue]];
+        id wegPushOptInKey = integration[WEG_PUSH_OPT_IN_KEY];
+        if (wegPushOptInKey) {
+            [user setOptInStatusForChannel:WEGEngagementChannelPush status:[wegPushOptInKey boolValue]];
         }
         
-        if (traits[WEG_SMS_OPT_IN_KEY] && [traits[WEG_SMS_OPT_IN_KEY] isKindOfClass:[NSNumber class]]) {
-            [user setOptInStatusForChannel:WEGEngagementChannelSMS status:[traits[WEG_SMS_OPT_IN_KEY] boolValue]];
+        id wegSmsOptInKey = integration[WEG_SMS_OPT_IN_KEY];
+        if (wegSmsOptInKey) {
+            [user setOptInStatusForChannel:WEGEngagementChannelSMS status:[wegSmsOptInKey boolValue]];
         }
         
-        if (traits[WEG_EMAIL_OPT_IN_KEY] && [traits[WEG_EMAIL_OPT_IN_KEY] isKindOfClass:[NSNumber class]]) {
-            [user setOptInStatusForChannel:WEGEngagementChannelEmail status:[traits[WEG_EMAIL_OPT_IN_KEY] boolValue]];
+        id wegEmailOptInKey = integration[WEG_EMAIL_OPT_IN_KEY];
+        if (wegEmailOptInKey) {
+            [user setOptInStatusForChannel:WEGEngagementChannelEmail status:[wegEmailOptInKey boolValue]];
         }
         
-        if (traits[WEG_SEGMENT_ADDRESS_KEY] && [traits[WEG_SEGMENT_ADDRESS_KEY] isKindOfClass:[NSDictionary class]]) {
-            
-            NSDictionary* address = traits[WEG_SEGMENT_ADDRESS_KEY];
-            
+        NSDictionary* address = integration[WEG_SEGMENT_ADDRESS_KEY];
+        if (address) {
             [address enumerateKeysAndObjectsUsingBlock:^(NSString* key, id obj, BOOL*  stop) {
-                
                 if ([obj isKindOfClass:[NSNumber class]]) {
                     [user setAttribute:key withValue:obj];
                 } else if ([obj isKindOfClass:[NSDate class]]) {
@@ -181,8 +163,8 @@
                 
             }];
         }
-        
-        
+
+        // Do we not want to remove WEG_SEGMENT_FIRST_NAME_KEY as well?
         [traitsCopy removeObjectForKey:WEG_SEGMENT_LAST_NAME_KEY];
         [traitsCopy removeObjectForKey:WEG_SEGMENT_NAME_KEY];
         [traitsCopy removeObjectForKey:WEG_SEGMENT_EMAIL_KEY];
@@ -199,7 +181,6 @@
         
         
         [traitsCopy enumerateKeysAndObjectsUsingBlock:^(NSString* key, id obj, BOOL*  stop) {
-            
             if ([obj isKindOfClass:[NSNumber class]]) {
                 [user setAttribute:key withValue:obj];
             } else if ([obj isKindOfClass:[NSDate class]]) {
@@ -217,16 +198,13 @@
     }
 }
 
-
 - (void)track:(SEGTrackPayload *)payload {
     
     NSString* eventName = payload.event;
-    NSDictionary* data = [payload.properties copy];
     
-    if (eventName && eventName.length > 0) {
-        
-        if (data) {
-            [[WebEngage sharedInstance].analytics trackEventWithName:eventName andValue:data];
+    if (eventName) {
+        if (payload.properties) {
+            [[WebEngage sharedInstance].analytics trackEventWithName:eventName andValue:payload.properties];
         } else {
             [[WebEngage sharedInstance].analytics trackEventWithName:eventName];
         }
@@ -234,11 +212,7 @@
 
 }
 
-
 - (void)screen:(SEGScreenPayload *)payload {
-
-    NSString* screenName = payload.name;
-    NSString* category = payload.category;
     
     NSMutableDictionary* data = [payload.properties mutableCopy];
     
@@ -249,14 +223,14 @@
         hasData = NO;
     }
     
-    if (category && category.length > 0 ) {
-        
+    NSString* category = payload.category;
+    if (category ) {
         [data setObject:category forKey:@"category"];
         hasCategory = YES;
     }
     
-    if (screenName && screenName.length > 0) {
-        
+    NSString* screenName = payload.name;
+    if (screenName) {
         if (hasData || hasCategory) {
             [[WebEngage sharedInstance].analytics navigatingToScreenWithName:screenName andData:data];
         } else {
@@ -271,21 +245,15 @@
 
 -(void)reset {
     [[WebEngage sharedInstance].user logout];
-    NSLog(@"[[WebEngage sharedInstance].user logout]");
+    SEGLog(@"[[WebEngage sharedInstance].user logout]");
 }
 
 - (void)group:(SEGGroupPayload *)payload {
-    NSLog(@"WebEngage SDK does not support the `group` operation");
+    SEGLog(@"WebEngage SDK does not support the `group` operation");
 }
-
 
 - (void)alias:(SEGAliasPayload *)payload {
-    NSLog(@"WebEngage SDK does not support the `alias` operation");
+    SEGLog(@"WebEngage SDK does not support the `alias` operation");
 }
-
-
-
-
-
 
 @end
