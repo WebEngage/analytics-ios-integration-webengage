d56bfccbe18441480904647995ba76ced38d5257
Uzma Sayyed <uzma.sayyed@webklipper.com>
2017-10-09 18:24:39 +0530

    Added two tests in test script

--- Example/Tests/Tests.m
+++ Example/Tests/Tests.m
@@ -2,13 +2,23 @@
 //  Segment-WebEngageTests.m
 //  Segment-WebEngageTests
 //
-//  Created by weg-arpit on 06/23/2017.
+//  Created by Uzma Sayyed on 10/9/2017.
 //  Copyright (c) 2017 weg-arpit. All rights reserved.
 //
 
+#import <WEGSegmentIntegration.h>
+#import <SEGAnalytics.h>
+#import <OCMock.h>
+#import <SEGTrackPayload.h>
+
 @import XCTest;
 
-@interface Tests : XCTestCase
+@interface Tests : XCTestCase{
+    SEGAnalyticsConfiguration *configuration;
+    __block id wegClassMock;
+    __block WEGUser* wegUserMock;
+    __block id<WEGAnalytics> wegAnalyticsMock;
+}
 
 @end
 
@@ -18,6 +28,17 @@
 {
     [super setUp];
     // Put setup code here. This method is called before the invocation of each test method in the class.
+    
+    configuration = [SEGAnalyticsConfiguration configurationWithWriteKey:@"D56SbZAWtEIDvBbxuHchMGEx5WkvzP2U"];
+    configuration.trackApplicationLifecycleEvents = YES;
+    configuration.recordScreenViews = YES;
+    [SEGAnalytics setupWithConfiguration:configuration];
+    
+    wegClassMock=OCMClassMock([WebEngage class]);
+    OCMStub([wegClassMock sharedInstance]).andReturn(wegClassMock);
+    wegAnalyticsMock = OCMProtocolMock(@protocol(WEGAnalytics));
+    OCMStub([wegClassMock user]).andReturn(wegUserMock);
+    OCMStub([wegClassMock analytics]).andReturn(wegAnalyticsMock);
 }
 
 - (void)tearDown
@@ -26,10 +47,37 @@
     [super tearDown];
 }
 
-- (void)testExample
+- (void)testInitialization
 {
-    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+    
 }
 
+-(void)testTackEvent
+{
+    NSDictionary *properties=@{@"price":@"100",@"product name":@"test product1"};
+    [[SEGAnalytics sharedAnalytics] track:@"test event" properties:properties];
+    WEGSegmentIntegration* instance = [[WEGSegmentIntegration alloc] init];
+    SEGTrackPayload *payload=[[SEGTrackPayload alloc] initWithEvent:@"test event" properties:properties context:nil integrations:nil];
+    [instance track:payload];
+    OCMVerify(
+              [wegAnalyticsMock trackEventWithName:[OCMArg isEqual:@"test event"] andValue:[OCMArg checkWithBlock:^BOOL(id obj) {
+        NSLog(@"Verify %@ has correct values",obj);
+        return YES;
+    }]]);
+}
+
+
+-(void)testIdentify
+{
+    NSDictionary* traits=@{@"first_name":@"test",@"email":@"test@test.com",@"gender":@"female",@"phone":@"0918273645"};
+    [[SEGAnalytics sharedAnalytics] identify:@"test123" traits:traits];
+    WEGSegmentIntegration* instance = [[WEGSegmentIntegration alloc] init];
+    SEGIdentifyPayload* payload=[[SEGIdentifyPayload alloc] initWithUserId:@"test123" anonymousId:nil traits:traits context:nil integrations:nil];
+    [instance identify:payload];
+    OCMVerify([wegUserMock setFirstName:[OCMArg isEqual:@"test"]]);
+    OCMVerify([wegUserMock setEmail:[OCMArg isEqual:@"test@test.com"]]);
+    OCMVerify([wegUserMock setPhone:[OCMArg isEqual:@"0918273645"]]);
+    OCMVerify([wegUserMock setGender:[OCMArg isEqual:@"female"]]);
+}
 @end
 
